b6eba6068aca0e52d4de7ded660fbe1340d2c472
gatosyocora <gatosyocora36@gmail.com>
2021-03-07 23:10:56 +0900

    Avatarに関するUtilityクラスを作成

---
+++ Editor/Utilities/VRCAvatarUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Animations;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+
+namespace VRCAvatars3Validator.Utilities
+{
+    public static class VRCAvatarUtility
+    {
+        public static AnimatorController[] GetControllers(VRCAvatarDescriptor avatar)
+            => avatar.baseAnimationLayers
+                .Union(avatar.specialAnimationLayers)
+                .Select(l => l.animatorController as AnimatorController)
+                .Where(c => c != null)
+                .ToArray();
+    }
+}
