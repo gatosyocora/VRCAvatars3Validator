fde5864cc7de3901c5e76b47b66b96f4f53d4d9e
gatosyocora <gatosyocora36@gmail.com>
2021-03-11 03:34:42 +0900

    ルールスクリプトの作成のためにアセットを右クリックでGUIDを取得できるようにするEditor拡張を追加

---
+++ Editor/Utilities/UtilityExtensionTools.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class UtilityExtensionTools : Editor
+{
+    [MenuItem("Assets/Copy GUID")]
+    public static void CopyGUID()
+    {
+        var asset = Selection.activeObject;
+        var path = AssetDatabase.GetAssetPath(asset);
+        var guid = AssetDatabase.AssetPathToGUID(path);
+        GUIUtility.systemCopyBuffer = guid;
+    }
+}
