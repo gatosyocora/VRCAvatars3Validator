791b012d904f1c1a51a42cff014fb5c862b6e911
gatosyocora <gatosyocora36@gmail.com>
2021-05-15 17:10:44 +0900

    データ部分と管理部分を分離

---
+++ Editor/ValidatorSettingsService.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+
+namespace VRCAvatars3Validator
+{
+    public class ValidatorSettingsService : Editor
+    {
+        private const string SETTINGS_FILE_PATH = "Assets/VRCAvatars3Validator/Editor/Settings.asset";
+
+        private ValidatorSettings _validatorSettings;
+
+        public void OnEnable()
+        {
+            if (_validatorSettings == null)
+            {
+                _validatorSettings = GetOrCreateSettings();
+            }
+
+            if (_validatorSettings.validateRuleDictionary.Count <= 0)
+            {
+                var ruleNames = RuleManager.GetRuleNames();
+
+                foreach (var ruleName in ruleNames)
+                {
+                    _validatorSettings.validateRuleDictionary.Add(ruleName, true);
+                }
+            }
+        }
+
+        public static ValidatorSettings GetOrCreateSettings()
+        {
+            ValidatorSettings settings;
+            settings = AssetDatabase.LoadAssetAtPath<ValidatorSettings>(SETTINGS_FILE_PATH);
+            if (settings) return settings;
+
+            settings = CreateInstance<ValidatorSettings>();
+            AssetDatabase.CreateAsset(settings, SETTINGS_FILE_PATH);
+            AssetDatabase.SaveAssets();
+            return settings;
+        }
+    }
+}
