c395033ba4db40285bf45afc2c6d30c381eea3c6
gatosyocora <gatosyocora36@gmail.com>
2021-05-24 22:54:44 +0900

    Exporterを修正

---
+++ Assets/Editor/UnityPackageExporter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+
+public static class UnityPackageExporter
+{
+    public static void Export()
+    {
+        var exportDirs = new[]
+        {
+            "Assets/VRCAvatars3Validator"
+        };
+        var exportPath = "./VRCAvatars3Validator.unitypackage";
+
+        AssetDatabase.ExportPackage(
+            exportDirs,
+            exportPath,
+            ExportPackageOptions.Recurse);
+    }
+}
