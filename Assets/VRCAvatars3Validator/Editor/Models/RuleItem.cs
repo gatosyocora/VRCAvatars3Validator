fe2a6c502a5e4ff10d7a020ab32cb865d2abb77f
Narazaka <info@narazaka.net>
2021-05-30 20:45:55 +0900

    load write

--- Assets/VRCAvatars3Validator/Editor/Models/RuleItem.cs
+++ Assets/VRCAvatars3Validator/Editor/Models/RuleItem.cs
@@ -7,5 +7,15 @@
         public string FilePath;
         public bool Enabled;
         public string Options;
+
+        public T LoadOptions<T>() where T : class {
+            if (Options == null || Options == "")
+                return null;
+            return UnityEngine.JsonUtility.FromJson<T>(Options);
+        }
+
+        public void WriteOptions(object options) {
+            Options = UnityEngine.JsonUtility.ToJson(options);
+        }
     }
 }
