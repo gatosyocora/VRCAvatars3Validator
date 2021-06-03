a1aa4395960badb400c19de5a5da1089d2033f3a
gatosyocora <gatosyocora36@gmail.com>
2021-06-04 01:22:52 +0900

    翻訳の仕組みを作成

---
+++ Assets/VRCAvatars3Validator/Editor/Models/LanguagePack.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace VRCAvatars3Validator.Models
+{
+    [CreateAssetMenu(menuName = "VRCAvatars3Validator/LanguagePack")]
+    public class LanguagePack : ScriptableObject
+    {
+        [TextArea(100, 100)]
+        public string data =
+        "{\n" +
+            "\"dictionary\":[\n" + 
+            "{\"key\":\"hogehoge\",\"value\":\"ほげほげ\"},\n" +
+            "]\n" +
+        "}";
+    }
+}
---
+++ Assets/VRCAvatars3Validator/Editor/UseCases/Localize.cs
@@ -0,0 +1,34 @@
+﻿using Kogane;
+using System;
+using UnityEditor;
+using UnityEngine;
+using VRCAvatars3Validator.Models;
+
+namespace VRCAvatars3Validator 
+{
+    public class Localize
+    {
+        public static string LANGUAGE_PACK_FOLDER = "Assets/VRCAvatars3Validator/Editor/Langs/";
+
+        public static LanguageType languageType = LanguageType.EN;
+
+        public static LanguagePack languagePack;
+
+        public static JsonDictionary translateDictionary;
+
+        public static string Tr(string textId)
+        {
+            if (languagePack == null)
+            {
+                languagePack = AssetDatabase.LoadAssetAtPath<LanguagePack>($"{LANGUAGE_PACK_FOLDER}{languageType}.asset");
+                Debug.Log($"{LANGUAGE_PACK_FOLDER}{languageType}.asset");
+            }
+
+            if (translateDictionary == null)
+            {
+                translateDictionary = JsonUtility.FromJson<JsonDictionary>(languagePack.data);
+            }
+            return translateDictionary[textId];
+        }
+    }
+}
